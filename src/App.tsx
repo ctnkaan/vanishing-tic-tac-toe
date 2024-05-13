988cb4cc1b9103c0bc5ddae083568bc5741d80c6
ctnkaan <kaan.taskingenc@gmail.com>
2024-05-13 21:44:27 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+    const gameState = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ]
+
+    return (
+        <>
+            <div
+                style={{
+                    width: '100px',
+                    height: '100px',
+                    border: '1px solid red',
+                }}
+            ></div>
+        </>
+    )
+}
+
+export default App
